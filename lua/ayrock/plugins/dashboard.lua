35f2748a669e86dcbcf0507863e344c3f08f2910
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:57 -0800

    -oil +mini.files

--- lua/ayrock/plugins/dashboard.lua
+++ lua/ayrock/plugins/dashboard.lua
@@ -4,11 +4,11 @@ return {
     local alpha = require('alpha')
     local dashboard = require('alpha.themes.dashboard')
     dashboard.section.buttons.val = {
-      dashboard.button('e', '󰙅 Explore', '-'),
-      dashboard.button('f', ' Find File', ':Telescope find_files<CR>'),
-      dashboard.button('g', ' Grep / Text Search', ':Telescope live_grep<CR>'),
-      dashboard.button('n', ' New File', ':ene | startinsert<CR>'),
-      dashboard.button('r', ' Recent Files', ':Telescope oldfiles<CR>'),
+      dashboard.button('-', '󰙅 Explore', ':lua MiniFiles.open()<CR>'),
+      dashboard.button('<leader>sf', ' Find File', ':Telescope find_files<CR>'),
+      dashboard.button('<leader>sg', ' Grep / Text Search', ':Telescope live_grep<CR>'),
+      dashboard.button('<leader>ene', ' New File', ':ene | startinsert<CR>'),
+      dashboard.button('<leader>so', ' Recent Files', ':Telescope oldfiles<CR>'),
       dashboard.button('q', ' Quit', ':qa<CR>'),
     }
     alpha.setup(dashboard.config)
---
+++ lua/ayrock/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'echasnovski/mini.files',
+    version = '*',
+    opts = {},
+    keys = {
+      { '<leader>-', ':lua MiniFiles.open()<cr>', desc = '[-] Explore' },
+      { '<leader>E', ':lua MiniFiles.open()<cr>', desc = '[E]xplore' },
+    },
+  },
+}
