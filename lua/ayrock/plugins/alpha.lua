c799f5c7b62a1832f8b996ed2e1106f1dbb446d6
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:57 -0800

    rename files to match plugin names

---
+++ lua/ayrock/plugins/alpha.lua
@@ -0,0 +1,28 @@
+return {
+  'goolord/alpha-nvim',
+  event = 'VimEnter',
+  opts = function()
+    local dashboard = require('alpha.themes.dashboard')
+    dashboard.section.buttons.val = {
+      dashboard.button('f', ' ' .. ' Find file', '<cmd> Telescope find_files <cr>'),
+      dashboard.button('n', ' ' .. ' New file', [[<cmd> ene <BAR> startinsert <cr>]]),
+      dashboard.button('r', ' ' .. ' Recent files', '<cmd> Telescope oldfiles <cr>'),
+      dashboard.button('g', ' ' .. ' Find text', '<cmd> Telescope live_grep <cr>'),
+      dashboard.button('x', '󰒲 ' .. ' Lazy', '<cmd> Lazy <cr>'),
+      dashboard.button('q', ' ' .. ' Quit', '<cmd> qa <cr>'),
+    }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = 'AlphaButtons'
+      button.opts.hl_shortcut = 'AlphaShortcut'
+    end
+    dashboard.section.header.opts.hl = 'AlphaHeader'
+    dashboard.section.buttons.opts.hl = 'AlphaButtons'
+    dashboard.section.footer.opts.hl = 'AlphaFooter'
+    dashboard.opts.layout[1].val = 8
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    local alpha = require('alpha')
+    alpha.setup(dashboard.config)
+  end,
+}
--- lua/ayrock/plugins/mini-files.lua
+++ lua/ayrock/plugins/mini-files.lua
@@ -4,6 +4,7 @@ return {
     version = '*',
     opts = {},
     keys = {
+      { '-', ':lua MiniFiles.open()<cr>', desc = '[-] Explore' },
       { '<leader>-', ':lua MiniFiles.open()<cr>', desc = '[-] Explore' },
       { '<leader>E', ':lua MiniFiles.open()<cr>', desc = '[E]xplore' },
     },
