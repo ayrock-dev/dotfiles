2679ce541dc5562a69fcd4a28b8b74fa1d56ba12
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:57 -0800

    configure conform

--- lua/ayrock/plugins/format.lua
+++ lua/ayrock/plugins/format.lua
@@ -12,19 +12,24 @@ return {
     },
   },
   opts = {
+    stop_after_first = true,
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style.
-      local disable_filetypes = { c = true, cpp = true }
       return {
         timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        lsp_fallback = true,
       }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { { 'prettierd', 'prettier' } },
+      javascript = { 'prettierd', 'prettier' },
+      typescript = { 'prettierd', 'prettier' },
+      javascriptreact = { 'prettierd', 'prettier' },
+      typescriptreact = { 'prettierd', 'prettier' },
+      html = { 'prettierd', 'prettier' },
+      json = { 'prettierd', 'prettier' },
+      jsonc = { 'prettierd', 'prettier' },
+      graphql = { 'prettierd', 'prettier' },
     },
   },
 }
