4cb812f98e5af46349eab1dc34f2a063bd05d088
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:58 -0800

    migrate to gnu stow

--- README.md
+++ README.md
@@ -1,49 +1,11 @@
-# ayrock-nvim
+# dotfiles
 
-A nvim configuration based on [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
-some edits
-## Installation
+```bash
+cd ~/dev
 
-> ⚠️ Backup your previous configuration (if any exists)
-#### Requirements
+git clone git@github.com:ayrock-dev/dotfiles
 
-[telescope](https://github.com/nvim-telescope/telescope.nvim#suggested-dependencies) requires [ripgrep](https://github.com/BurntSushi/ripgrep#installation).
+cd dotfiles
 
-```sh
-brew install ripgrep
+./install
 ```
-
-Neovim's configurations are located at `~/.config/nvim`. To clone `ayrock-nvim` to this folder, use the following.
-
-```sh
-git clone https://github.com/ayrock-dev/ayrock-nvim.git ~/.config/nvim
-```
-
-## Post Installation
-
-Start Neovim via
-
-```sh
-nvim
-```
-
-The `Lazy` plugin manager will start automatically on the first run and install the configured plugins - as can be seen in the introduction video. After the installation is complete you can press `q` to close the `Lazy` UI and **you are ready to go**! Next time you run nvim `Lazy` will no longer show up.
-
-## Configuration And Extension
-
-- To add plugins, add new file in `lua/ayrock/plugins/*`, which will be auto sourced.
-
-## `Lazy` Usage
-
-- `opts = {}` is the same as calling `setup({})`
-
-## Ayrock Dotfiles
-
-`~/.zshrc`
-
-```
-PS1="%n %1~ λ "
-
-alias vim='nvim'
-```
-
---
+++ home/.config/nvim/lua/ayrock/plugins/snacks.lua
@@ -0,0 +1,76 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = ' ', key = 'M', desc = 'Mason', action = ':Mason' },
+          { icon = '󰒲 ', key = 'L', desc = 'Lazy Plugins', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { section = 'startup' },
+      },
+    },
+    notifier = {},
+  },
+  config = function(opts)
+    -- lsp notifications
+
+    --@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+    local progress = vim.defaulttable()
+    vim.api.nvim_create_autocmd('LspProgress', {
+      ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        if not client or type(value) ~= 'table' then
+          return
+        end
+        local p = progress[client.id]
+
+        for i = 1, #p + 1 do
+          if i == #p + 1 or p[i].token == ev.data.params.token then
+            p[i] = {
+              token = ev.data.params.token,
+              msg = ('[%3d%%] %s%s'):format(
+                value.kind == 'end' and 100 or value.percentage or 100,
+                value.title or '',
+                value.message and (' **%s**'):format(value.message) or ''
+              ),
+              done = value.kind == 'end',
+            }
+            break
+          end
+        end
+
+        local msg = {} ---@type string[]
+        progress[client.id] = vim.tbl_filter(function(v)
+          return table.insert(msg, v.msg) or not v.done
+        end, p)
+
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        vim.notify(table.concat(msg, '\n'), 'info', {
+          id = 'lsp_progress',
+          title = client.name,
+          opts = function(notif)
+            notif.icon = #progress[client.id] == 0 and ' ' or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          end,
+        })
+      end,
+    })
+
+    return opts
+  end,
+}
