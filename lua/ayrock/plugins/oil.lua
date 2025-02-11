5499fe1708bc5c29d3b35ed194f448d7d493bacf
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:57 -0800

    configure oil the lazy way

--- lua/ayrock/plugins/oil.lua
+++ lua/ayrock/plugins/oil.lua
@@ -1,23 +1,34 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('oil').setup({
-      columns = { 'icon' },
-      keymaps = {
-        ['<C-h>'] = false,
-        ['<M-h>'] = 'actions.select_split',
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    })
-
-    -- Open parent directory in current window
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = '[-] Explore' })
-    vim.keymap.set('n', '<Leader>E', '<CMD>Oil<CR>', { desc = '[E]xplore' })
-
-    -- Open parent directory in floating window
-    vim.keymap.set('n', '<Leader>-', require('oil').toggle_float, { desc = '[-] Explore' })
-  end,
+  opts = {
+    default_file_explorer = true,
+    columns = { 'icon' },
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  keys = {
+    {
+      '-',
+      function()
+        require('oil').open()
+      end,
+      desc = '[-] Explore',
+    },
+    {
+      '<leader>E',
+      function()
+        require('oil').open()
+      end,
+      desc = '[E]xplore',
+    },
+    {
+      '<leader>-',
+      function()
+        require('oil').toggle_float()
+      end,
+      desc = '[-] Explore (Popup)',
+    },
+  },
 }
