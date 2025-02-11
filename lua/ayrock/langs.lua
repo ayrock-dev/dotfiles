0d0bf8efa5b2426e3c490c0fccad794271d9bf07
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:56 -0800

    make a space to manage language support

---
+++ lua/ayrock/langs.lua
@@ -0,0 +1,48 @@
+-- Language support configuration.
+
+-- This file is a space to manage language support.
+-- This includes config for both treesitter and mason_lspconfig.
+
+-- To add support for a language, look up the treesitter module as well
+-- as the language server.
+
+return {
+  -- treesitter syntax highlighting
+  -- See: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+  treesitter_langs = {
+    'lua',
+    'rust',
+    'tsx',
+    'javascript',
+    'typescript',
+    'dockerfile',
+    'bash',
+    'json',
+    'yaml',
+    'toml',
+    'prisma',
+    'proto',
+  },
+  -- language servers
+  -- See: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+  servers = {
+    lua_ls = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+        -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        -- diagnostics = { disable = { 'missing-fields' } },
+      },
+    },
+    rust_analyzer = {},
+    tsserver = {},
+    html = {},
+    htmx = {},
+    dockerls = {}, -- Dockerfile
+    jsonls = {},
+    yamlls = {},
+    taplo = {}, -- .toml
+    prismals = {}, -- .prsima
+    tailwindcss = {},
+  },
+}
