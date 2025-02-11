2a3ca027c430556f75031528f839dfe89a8f3fe9
ayrock-dev <ayrocklee@gmail.com>
2025-02-10 23:03:58 -0800

    housecleaning

--- lua/ayrock/plugins/treesitter.lua
+++ lua/ayrock/plugins/treesitter.lua
@@ -5,11 +5,20 @@ return {
   },
   build = ':TSUpdate',
   opts = {
-    -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = require('ayrock/langs').treesitter_langs,
     auto_install = false,
-    highlight = { enable = true },
+    highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100kb
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
     indent = { enable = true },
+    additional_vim_regex_highlighting = false, -- applies to langs such as ruby or php
   },
   config = function(_, opts)
     require('nvim-treesitter.install').prefer_git = true
