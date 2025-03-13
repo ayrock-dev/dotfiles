ee3fad2cebe8bc84d5678273e2f0cb9f3ff20841
Eric Lee <eric.lee@discordapp.com>
2025-03-12 17:03:49 -0700

    add folke todo to the picker

--- home/.config/nvim/lua/ayrock/plugins/comment.lua
+++ home/.config/nvim/lua/ayrock/plugins/comment.lua
@@ -1,5 +1,17 @@
 return {
-  { 'folke/todo-comments.nvim' },
+  {
+    'folke/todo-comments.nvim',
+    optional = true,
+    keys = {
+      {
+        '<leader>st',
+        function()
+          Snacks.picker.todo_comments({ keywords = { 'todo', 'Todo', 'TODO', 'FIX', 'FIXME' } })
+        end,
+        desc = '[s]earch [t]odo comments',
+      },
+    },
+  },
   {
     -- "gc" to comment visual regions/lines
     'numToStr/Comment.nvim',
