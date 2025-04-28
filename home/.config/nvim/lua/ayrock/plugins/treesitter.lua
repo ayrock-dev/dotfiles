1883f1a18d18f9d679d739af5085caad3ee3157b
Eric Lee <eric.lee@discordapp.com>
2025-04-28 11:27:58 -0700

    larger max filesize for treesitter

--- home/.config/nvim/lua/ayrock/plugins/treesitter.lua
+++ home/.config/nvim/lua/ayrock/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
     highlight = {
       enable = true,
       disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100kb
+        local max_filesize = 1000 * 1024 -- 1000kb
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
