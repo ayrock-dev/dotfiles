f294e21f420ca8e2a3ba33b80c3a202ea49721d3
Eric Lee <eric.lee@discordapp.com>
2025-07-06 20:46:45 -0700

    fix file path

--- home/.config/nvim/lua/ayrock/plugins/lualine.lua
+++ home/.config/nvim/lua/ayrock/plugins/lualine.lua
@@ -28,11 +28,21 @@ return {
         lualine_b = { 'branch' },
         lualine_c = {
           {
+            'filename',
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false,
             -- 0: Just the filename
-            path = 1, -- 1: Relative path
             -- 2: Absolute path
             -- 3: Absolute path, with tilde as the home directory
             -- 4: Filename and parent dir, with tilde as the home directory
+            path = 1, -- 1: Relative path
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            symbols = {
+              modified = '[+]', -- Text to show when the file is modified.
+              readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+              unnamed = '[No Name]', -- Text to show for unnamed buffers.
+              newfile = '[New]', -- Text to show for newly created file before first write
+            },
           },
           {
             'diagnostics',
