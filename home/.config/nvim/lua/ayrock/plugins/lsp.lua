8040a5eb1ef324603b72d19a8cfde96467629862
ayrock-dev <ayrocklee@gmail.com>
2025-08-07 09:16:40 -0700

    bring back virtual text diagnostics

--- home/.config/nvim/lua/ayrock/plugins/lsp.lua
+++ home/.config/nvim/lua/ayrock/plugins/lsp.lua
@@ -58,6 +58,37 @@ return {
       end,
     })
 
+    local icons = require('ayrock/icons')
+
+    -- Diagnostic Config
+    -- See :help vim.diagnostic.Opts
+    vim.diagnostic.config({
+      severity_sort = true,
+      float = { border = 'rounded', source = 'if_many' },
+      underline = { severity = vim.diagnostic.severity.ERROR },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+          [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
+          [vim.diagnostic.severity.INFO] = icons.diagnostics.info,
+          [vim.diagnostic.severity.HINT] = icons.diagnostics.info,
+        },
+      },
+      virtual_text = {
+        source = 'if_many',
+        spacing = 2,
+        format = function(diagnostic)
+          local diagnostic_message = {
+            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+            [vim.diagnostic.severity.WARN] = diagnostic.message,
+            [vim.diagnostic.severity.INFO] = diagnostic.message,
+            [vim.diagnostic.severity.HINT] = diagnostic.message,
+          }
+          return diagnostic_message[diagnostic.severity]
+        end,
+      },
+    })
+
     local servers = require('ayrock/langs').servers
     local ensure_installed = vim.tbl_keys(servers)
     require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
