019619145575e6667abe4025ecb1278d62f28017
Eric Lee <eric.lee@discordapp.com>
2025-08-14 10:16:12 -0700

    mv supermaven out of blink

--- home/.config/nvim/lua/ayrock/plugins/completion.lua
+++ home/.config/nvim/lua/ayrock/plugins/completion.lua
@@ -3,31 +3,17 @@ return {
     'saghen/blink.cmp',
     lazy = false,
     version = '1.*',
-    dependencies = {
-      {
-        'saghen/blink.compat',
-        version = '*',
-        lazy = true,
-        opts = {
-          impersonate_nvim_cmp = true,
-        },
-      },
-      {
-        'supermaven-inc/supermaven-nvim',
-        opts = {
-          disable_inline_completion = true,
-          disable_keymaps = true,
-        },
-      },
-    },
     event = 'InsertEnter',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        preset = 'super-tab',
-        ['<C-y>'] = { 'select_and_accept' },
+        -- https://cmp.saghen.dev/configuration/keymap.html#presets
+        preset = 'default',
+      },
+      appearance = {
+        nerd_font_variant = 'mono',
       },
       completion = {
         accept = {
@@ -40,30 +26,17 @@ return {
             treesitter = { 'lsp' },
           },
         },
-        trigger = {
-          show_on_keyword = true,
-        },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 200,
         },
       },
       sources = {
-        default = { 'supermaven', 'lsp', 'path' },
-        providers = {
-          supermaven = {
-            name = 'supermaven',
-            module = 'blink.compat.source',
-            async = true,
-            transform_items = function(_, items)
-              for _, item in ipairs(items) do
-                item.kind_icon = '󰘧'
-                item.kind_name = 'Supermaven'
-              end
-              return items
-            end,
-            score_offset = 10,
-          },
+        -- intentionally omit snippets, I don't use them
+        default = {
+          'lsp',
+          'path',
+          'buffer',
         },
       },
 
