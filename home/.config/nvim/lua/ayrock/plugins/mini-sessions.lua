c17ab771b0d30cad391073675cc3c8a71245dc7b
Eric Lee <eric.lee@discordapp.com>
2026-03-29 12:56:37 -0700

    save session on <leader>ss

--- home/.config/nvim/lua/ayrock/plugins/mini-sessions.lua
+++ home/.config/nvim/lua/ayrock/plugins/mini-sessions.lua
@@ -1,3 +1,10 @@
 return {
-  { 'echasnovski/mini.sessions', version = false, lazy = false },
+  {
+    'echasnovski/mini.sessions',
+    version = false,
+    lazy = false,
+    keys = {
+      ['<leader>ss'] = { ':mksession!', mode = { 'n', 'x' }, desc = 'Save Session' },
+    },
+  },
 }
