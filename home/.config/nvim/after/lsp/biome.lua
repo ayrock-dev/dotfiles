1581af3bfcbcc00e6be261d5b703add47b4d32b5
Eric Lee <eric.lee@discordapp.com>
2026-04-13 14:21:06 -0700

    format json in dotfiles

--- home/.config/nvim/after/lsp/biome.lua
+++ home/.config/nvim/after/lsp/biome.lua
@@ -1,10 +1,6 @@
 ---@type vim.lsp.Config
 return {
   root_dir = function(bufnr, on_dir)
-    if vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc', 'deno.lock' }) then
-      return
-    end
-
     local filename = vim.api.nvim_buf_get_name(bufnr)
     local biome_config = vim.fs.find({ 'biome.json', 'biome.jsonc' }, {
       path = filename,
@@ -17,10 +13,5 @@ return {
     end
 
     on_dir(vim.fs.dirname(biome_config))
-  end,
-  on_init = function(client)
-    if client.server_capabilities then
-      client.server_capabilities.documentFormattingProvider = false
-    end
   end,
 }
